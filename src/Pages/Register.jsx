557869ac2410e90e74240af708d0962dba2240f2
okuhlecharlieman <114597427+okuhlecharlieman@users.noreply.github.com>
2023-11-28 17:19:26 +0200

    Update Register.jsx

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,21 +1,28 @@
+import React, { useEffect } from "react";
+import { useWallet } from "@thirdweb-dev/react";
+import { useHistory } from "react-router-dom";
 import { ConnectWallet } from "@thirdweb-dev/react";
-import { useState } from "react";
 
+export default function Register() {
+ const { wallet } = useWallet();
+ const history = useHistory();
 
+ useEffect(() => {
+   if (wallet) {
+     history.push("/redirect-route");
+   }
+ }, [wallet, history]);
 
-export default function Component() {
-  
-    return (
-        < div className="bg-gradient-to-t from-green-500  to-cyan-400 h-screen">
-
-            <div className="flex w-full justify-center gap-4">
-<div><p>Register or login by connecting to your Metamask</p></div>
-              
-                <  ConnectWallet />
-              
-            </div>
-        </div>)
+ return (
+   <div className="bg-gradient-to-t from-green-500 to-cyan-400 h-screen">
+     <div className="flex w-full justify-center gap-4">
+       <div><p>Register or login by connecting to your Metamask</p></div>
+       <ConnectWallet />
+     </div>
+   </div>
+ );
 }
+
 
 
 
