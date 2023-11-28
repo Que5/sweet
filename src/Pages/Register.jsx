575c846f66812ff90d353618e80e022396a4d34c
okuhlecharlieman <114597427+okuhlecharlieman@users.noreply.github.com>
2023-11-28 17:13:02 +0200

    Update Register.jsx

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,24 +1,10 @@
 import { ConnectWallet } from "@thirdweb-dev/react";
 import { useState } from "react";
 
-import { useContract, useContractWrite } from "@thirdweb-dev/react";
+
 
 export default function Component() {
-    const [name, setName] = useState("")
-    const { contract } = useContract("0xEd1CD02Dc4782482c30c59392596151bDFFE904b");
-    const { mutateAsync: registerUser, isLoading } = useContractWrite(contract, "registerUser")
-
-    const call = async () => {
-        try {
-            const data = await registerUser({ args: [name] });
-            console.info("contract call successs", data);
-        } catch (err) {
-            console.error("contract call failure", err);
-        }
-    }
-    const handleUserChange = (e) => {
-        setName(e.target.value)
-    }
+  
     return (
         < div className="bg-gradient-to-t from-green-500  to-cyan-400 h-screen">
 
